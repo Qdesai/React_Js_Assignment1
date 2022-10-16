cfcc287fdc2f0fc4a107212a5598bf9d45a1d072
Qdesai <qaasimdesai786@gmail.com>
2022-10-15 23:22:09 -0400

    created the assignment and doing the first commit

---
+++ assignment-1/src/MyComponent.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+
+export default function MyCompenent(){
+    
+    const [temp, setCount]  = useState();
+
+    return(
+        <div>
+            <h1>Mohammed Qaasim Desai Assignment 1</h1>
+            <h2>Temprature Calculator</h2>            
+        </div>
+    )
+}
